25aa75f1f20d0fbb8ff2bf84f3cafebc8fd4ca8f
DevExpressExampleBot <fakeEmail>
2022-02-01 21:21:01 +0300

    Source auto update [skip ci]

--- VB/HighlightSearchResults/Properties/AssemblyInfo.vb
+++ VB/HighlightSearchResults/Properties/AssemblyInfo.vb
@@ -1,27 +1,23 @@
-﻿Imports System.Reflection
-Imports System.Runtime.CompilerServices
+Imports System.Reflection
 Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("Destination")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("Destination")>
-<Assembly: AssemblyCopyright("Copyright ©  2016")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
-
+<Assembly:AssemblyTitle("Destination")>
+<Assembly:AssemblyDescription("")>
+<Assembly:AssemblyConfiguration("")>
+<Assembly:AssemblyCompany("")>
+<Assembly:AssemblyProduct("Destination")>
+<Assembly:AssemblyCopyright("Copyright ©  2016")>
+<Assembly:AssemblyTrademark("")>
+<Assembly:AssemblyCulture("")>
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
 ' COM, set the ComVisible attribute to true on that type.
-<Assembly: ComVisible(False)>
-
+<Assembly:ComVisible(False)>
 ' The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("33979c75-3b72-4e01-b247-d40cabb2b4cc")>
-
+<Assembly:Guid("33979c75-3b72-4e01-b247-d40cabb2b4cc")>
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
@@ -32,5 +28,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 ' [assembly: AssemblyVersion("1.0.*")]
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly:AssemblyVersion("1.0.0.0")>
+<Assembly:AssemblyFileVersion("1.0.0.0")>
