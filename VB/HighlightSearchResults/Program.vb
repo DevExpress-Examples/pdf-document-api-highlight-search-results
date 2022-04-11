268c04fc7ef0892224a842713ef0da8c793d4685
DevExpressExampleBot <fakeEmail>
2022-04-11 11:54:28 +0400

    Source auto update [skip ci]

--- VB/HighlightSearchResults/Program.vb
+++ VB/HighlightSearchResults/Program.vb
@@ -6,40 +6,41 @@ Namespace HighlightSearchResults
     Friend Class Program
 
         Shared Sub Main(ByVal args As String())
-            'Create a PDF document processor.
+            ' Create a PDF document processor.
             Using documentProcessor As PdfDocumentProcessor = New PdfDocumentProcessor()
-                'Define search words
+                ' Define search words
                 Dim words As String() = {"Get", "DX-RX809", "HD", "DX-B5000"}
-                'Load a PDF document
+                ' Load a PDF document
                 documentProcessor.LoadDocument("..\..\Document.pdf")
-                'Specify the search parameters
+                ' Specify search parameters
                 Dim searchParameters As PdfTextSearchParameters = New PdfTextSearchParameters()
                 searchParameters.CaseSensitive = True
                 searchParameters.WholeWords = True
-                'Comment the following "using" statement if you use annotations
+                ' Comment the following "using" statement if you use annotations
                 Using brush = New SolidBrush(Color.FromArgb(130, 55, 155, 255))
                     For Each word As String In words
-                        'Get the search results from the FindText method call with search text and search parameters
+                        ' Get the search results from the FindText method call
+                        ' with search text and search parameters
                         Dim result As PdfTextSearchResults = documentProcessor.FindText(word, searchParameters)
-                        'Highlight the result
+                        ' Highlight the result
                         While result.Status = PdfTextSearchStatus.Found
                             Using graphics As PdfGraphics = documentProcessor.CreateGraphics()
                                 HighlightResult(graphics, result, brush)
                             End Using
 
-                            'Use this method call to add annotations:
-                            'HighlightResult(documentProcessor, result);
+                            ' Use this method call to add annotations:
+                            ' HighlightResult(documentProcessor, result);
                             result = documentProcessor.FindText(word, searchParameters)
                         End While
                     Next
                 End Using
 
-                'Save the document
+                ' Save the document
                 documentProcessor.SaveDocument("..\..\Result.pdf")
             End Using
         End Sub
 
-        'This method uses PdfGraphics to highlight text
+        ' This method uses PdfGraphics to highlight text
         Public Shared Sub HighlightResult(ByVal graphics As PdfGraphics, ByVal result As PdfTextSearchResults, ByVal brush As SolidBrush)
             For i As Integer = 0 To result.Rectangles.Count - 1
                 Dim rect As RectangleF = New RectangleF(New PointF(CSng(result.Rectangles(i).Left), CSng(result.Page.CropBox.Height) - CSng(result.Rectangles(i).Top)), New SizeF(CSng(result.Rectangles(i).Width), CSng(result.Rectangles(i).Height)))
@@ -49,7 +50,7 @@ Namespace HighlightSearchResults
             graphics.AddToPageForeground(result.Page, 72, 72)
         End Sub
 
-        'This method uses annotations to highlight text
+        ' This method uses annotations to highlight text
         Public Shared Sub HighlightResult(ByVal processor As PdfDocumentProcessor, ByVal result As PdfTextSearchResults)
             For i As Integer = 0 To result.Rectangles.Count - 1
                 Dim annotation As PdfTextMarkupAnnotationData = processor.AddTextMarkupAnnotation(result.PageNumber, result.Rectangles(i), PdfTextMarkupAnnotationType.Highlight)
