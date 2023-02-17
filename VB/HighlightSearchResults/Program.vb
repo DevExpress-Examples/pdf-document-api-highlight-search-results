09bdf82cf8df0c97714aa4e7b6feba63e9f917f2
DevExpressExampleBot <fakeEmail>
2023-02-17 18:29:33 +0400

    Source auto update [skip ci]

--- VB/HighlightSearchResults/Program.vb
+++ VB/HighlightSearchResults/Program.vb
@@ -16,23 +16,14 @@ Namespace HighlightSearchResults
                 Dim searchParameters As PdfTextSearchParameters = New PdfTextSearchParameters()
                 searchParameters.CaseSensitive = True
                 searchParameters.WholeWords = True
-                'Comment the following "using" statement if you use annotations
-                Using brush = New SolidBrush(Color.FromArgb(130, 55, 155, 255))
-                    For Each word As String In words
-                        'Get the search results from the FindText method call with search text and search parameters
-                        Dim result As PdfTextSearchResults = documentProcessor.FindText(word, searchParameters)
-                        'Highlight the result
-                        While result.Status = PdfTextSearchStatus.Found
-                            Using graphics As PdfGraphics = documentProcessor.CreateGraphics()
-                                HighlightResult(graphics, result, brush)
-                            End Using
-
-                            'Use this method call to add annotations:
-                            'HighlightResult(documentProcessor, result);
-                            result = documentProcessor.FindText(word, searchParameters)
-                        End While
-                    Next
-                End Using
+                For Each word As String In words
+                    Dim result As PdfTextSearchResults
+                    'Get the search results from the FindText method call with search text and search parameters
+                    While(CSharpImpl.__Assign(result, documentProcessor.FindText(word, searchParameters))).Status = PdfTextSearchStatus.Found
+                        'HighlightResultWithGraphics(documentProcessor, result);
+                        HighlightResultUsingAnnotations(documentProcessor, result)
+                    End While
+                Next
 
                 'Save the document
                 documentProcessor.SaveDocument("..\..\Result.pdf")
@@ -40,17 +31,21 @@ Namespace HighlightSearchResults
         End Sub
 
         'This method uses PdfGraphics to highlight text
-        Public Shared Sub HighlightResult(ByVal graphics As PdfGraphics, ByVal result As PdfTextSearchResults, ByVal brush As SolidBrush)
-            For i As Integer = 0 To result.Rectangles.Count - 1
-                Dim rect As RectangleF = New RectangleF(New PointF(CSng(result.Rectangles(i).Left), CSng(result.Page.CropBox.Height) - CSng(result.Rectangles(i).Top)), New SizeF(CSng(result.Rectangles(i).Width), CSng(result.Rectangles(i).Height)))
-                graphics.FillRectangle(brush, rect)
-            Next
+        Private Shared Sub HighlightResultWithGraphics(ByVal processor As PdfDocumentProcessor, ByVal result As PdfTextSearchResults)
+            Using graphics As PdfGraphics = processor.CreateGraphics()
+                For i As Integer = 0 To result.Rectangles.Count - 1
+                    Dim rect As RectangleF = New RectangleF(New PointF(CSng(result.Rectangles(i).Left), CSng(result.Page.CropBox.Height) - CSng(result.Rectangles(i).Top)), New SizeF(CSng(result.Rectangles(i).Width), CSng(result.Rectangles(i).Height)))
+                    Using brush = New SolidBrush(Color.FromArgb(130, 55, 155, 255))
+                        graphics.FillRectangle(brush, rect)
+                    End Using
+                Next
 
-            graphics.AddToPageForeground(result.Page, 72, 72)
+                graphics.AddToPageForeground(result.Page, 72, 72)
+            End Using
         End Sub
 
         'This method uses annotations to highlight text
-        Public Shared Sub HighlightResult(ByVal processor As PdfDocumentProcessor, ByVal result As PdfTextSearchResults)
+        Private Shared Sub HighlightResultUsingAnnotations(ByVal processor As PdfDocumentProcessor, ByVal result As PdfTextSearchResults)
             For i As Integer = 0 To result.Rectangles.Count - 1
                 Dim annotation As PdfTextMarkupAnnotationData = processor.AddTextMarkupAnnotation(result.PageNumber, result.Rectangles(i), PdfTextMarkupAnnotationType.Highlight)
                 If annotation IsNot Nothing Then
@@ -58,5 +53,14 @@ Namespace HighlightSearchResults
                 End If
             Next
         End Sub
+
+        Private Class CSharpImpl
+
+            <System.Obsolete("Please refactor calling code to use normal Visual Basic assignment")>
+            Shared Function __Assign(Of T)(ByRef target As T, value As T) As T
+                target = value
+                Return value
+            End Function
+        End Class
     End Class
 End Namespace
