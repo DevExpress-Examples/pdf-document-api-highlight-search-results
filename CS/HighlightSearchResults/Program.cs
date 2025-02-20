bf3a4b280fb77b4755431fa24da38b3e5db19fed
guemjalee <polina.fedorova@devexpress.com>
2025-02-20 15:17:38 +0100

    upgrade to dot net 8

--- CS/HighlightSearchResults/Program.cs
+++ CS/HighlightSearchResults/Program.cs
@@ -1,4 +1,6 @@
-﻿using DevExpress.Pdf;
+﻿using DevExpress.Drawing;
+using DevExpress.Pdf;
+using System.Diagnostics;
 using System.Drawing;
 
 namespace HighlightSearchResults
@@ -15,38 +17,39 @@ namespace HighlightSearchResults
                 string[] words = { "Get", "DX-RX809", "HD", "DX-B5000" };
 
                 //Load a PDF document
-                documentProcessor.LoadDocument(@"..\..\Document.pdf");
+                documentProcessor.LoadDocument(@"..\..\..\Document.pdf");
 
                 //Specify the search parameters
                 PdfTextSearchParameters searchParameters = new PdfTextSearchParameters();
                 searchParameters.CaseSensitive = true;
                 searchParameters.WholeWords = true;
 
-                foreach (string word in words) 
+                foreach (string word in words)
                 {
                     PdfTextSearchResults result;
                     //Get the search results from the FindText method call with search text and search parameters
-                    while ((result = documentProcessor.FindText(word, searchParameters)).Status == PdfTextSearchStatus.Found) 
+                    while ((result = documentProcessor.FindText(word, searchParameters)).Status == PdfTextSearchStatus.Found)
                     {
                         //HighlightResultWithGraphics(documentProcessor, result);
-                        HighlightResultUsingAnnotations(documentProcessor, result);
+                        HighlightResultWithAnnotations(documentProcessor, result);
                     }
                 }
                 //Save the document
-                documentProcessor.SaveDocument(@"..\..\Result.pdf");
+                documentProcessor.SaveDocument(@"..\..\..\Result.pdf");
+                Process.Start(new ProcessStartInfo(@"..\..\..\Result.pdf") { UseShellExecute = true });
             }
         }
 
         //This method uses PdfGraphics to highlight text
         static void HighlightResultWithGraphics(PdfDocumentProcessor processor, PdfTextSearchResults result)
         {
-            using (PdfGraphics graphics = processor.CreateGraphics()) 
+            using (PdfGraphics graphics = processor.CreateGraphics())
             {
                 for (int i = 0; i < result.Rectangles.Count; i++)
                 {
                     RectangleF rect = new RectangleF(new PointF((float)result.Rectangles[i].Left, (float)result.Page.CropBox.Height - (float)result.Rectangles[i].Top),
                         new SizeF((float)result.Rectangles[i].Width, (float)result.Rectangles[i].Height));
-                    using (var brush = new SolidBrush(Color.FromArgb(130, 55, 155, 255)))
+                    using (var brush = new DXSolidBrush(Color.FromArgb(130, 55, 155, 255)))
                         graphics.FillRectangle(brush, rect);
                 }
                 graphics.AddToPageForeground(result.Page, 72, 72);
@@ -54,12 +57,15 @@ namespace HighlightSearchResults
         }
 
         //This method uses annotations to highlight text
-        static void HighlightResultUsingAnnotations(PdfDocumentProcessor processor, PdfTextSearchResults result)
+        static void HighlightResultWithAnnotations(PdfDocumentProcessor processor, PdfTextSearchResults result)
         {
+            PdfDocumentFacade facade = processor.DocumentFacade;
+            PdfPageFacade page = facade.Pages[result.Page.GetPageIndex()];
+
             for (int i = 0; i < result.Rectangles.Count; i++)
             {
-                PdfTextMarkupAnnotationData annotation =
-                processor.AddTextMarkupAnnotation(result.PageNumber, result.Rectangles[i], PdfTextMarkupAnnotationType.Highlight);
+                PdfTextMarkupAnnotationFacade annotation =
+                          page.AddTextMarkupAnnotation(result.Rectangles[i], PdfTextMarkupAnnotationType.Highlight);
                 if (annotation != null)
                 {
                     annotation.Color = new PdfRGBColor(0.2, 0.6, 0);
--- VB/HighlightSearchResults/Program.vb
+++ VB/HighlightSearchResults/Program.vb
@@ -1,32 +1,36 @@
+Imports DevExpress.Drawing
 Imports DevExpress.Pdf
+Imports System.Diagnostics
 Imports System.Drawing
 
 Namespace HighlightSearchResults
-
-    Friend Class Program
-
+    Class Program
         Shared Sub Main(ByVal args As String())
             'Create a PDF document processor.
-            Using documentProcessor As PdfDocumentProcessor = New PdfDocumentProcessor()
+            Using documentProcessor As New PdfDocumentProcessor()
                 'Define search words
                 Dim words As String() = {"Get", "DX-RX809", "HD", "DX-B5000"}
+
                 'Load a PDF document
-                documentProcessor.LoadDocument("..\..\Document.pdf")
+                documentProcessor.LoadDocument("..\..\..\Document.pdf")
+
                 'Specify the search parameters
-                Dim searchParameters As PdfTextSearchParameters = New PdfTextSearchParameters()
+                Dim searchParameters As New PdfTextSearchParameters()
                 searchParameters.CaseSensitive = True
                 searchParameters.WholeWords = True
+
                 For Each word As String In words
                     Dim result As PdfTextSearchResults
                     'Get the search results from the FindText method call with search text and search parameters
-                    While(CSharpImpl.__Assign(result, documentProcessor.FindText(word, searchParameters))).Status = PdfTextSearchStatus.Found
-                        'HighlightResultWithGraphics(documentProcessor, result);
-                        HighlightResultUsingAnnotations(documentProcessor, result)
-                    End While
+                    Do While InlineAssignHelper(result, documentProcessor.FindText(word, searchParameters)).Status = PdfTextSearchStatus.Found
+                        'HighlightResultWithGraphics(documentProcessor, result)
+                        HighlightResultWithAnnotations(documentProcessor, result)
+                    Loop
                 Next
 
                 'Save the document
-                documentProcessor.SaveDocument("..\..\Result.pdf")
+                documentProcessor.SaveDocument("..\..\..\Result.pdf")
+                Process.Start(New ProcessStartInfo("..\..\..\Result.pdf") With {.UseShellExecute = True})
             End Using
         End Sub
 
@@ -34,33 +38,33 @@ Namespace HighlightSearchResults
         Private Shared Sub HighlightResultWithGraphics(ByVal processor As PdfDocumentProcessor, ByVal result As PdfTextSearchResults)
             Using graphics As PdfGraphics = processor.CreateGraphics()
                 For i As Integer = 0 To result.Rectangles.Count - 1
-                    Dim rect As RectangleF = New RectangleF(New PointF(CSng(result.Rectangles(i).Left), CSng(result.Page.CropBox.Height) - CSng(result.Rectangles(i).Top)), New SizeF(CSng(result.Rectangles(i).Width), CSng(result.Rectangles(i).Height)))
-                    Using brush = New SolidBrush(Color.FromArgb(130, 55, 155, 255))
+                    Dim rect As New RectangleF(New PointF(CSng(result.Rectangles(i).Left), CSng(result.Page.CropBox.Height - result.Rectangles(i).Top)),
+                        New SizeF(CSng(result.Rectangles(i).Width), CSng(result.Rectangles(i).Height)))
+                    Using brush = New DXSolidBrush(Color.FromArgb(130, 55, 155, 255))
                         graphics.FillRectangle(brush, rect)
                     End Using
                 Next
-
                 graphics.AddToPageForeground(result.Page, 72, 72)
             End Using
         End Sub
 
         'This method uses annotations to highlight text
-        Private Shared Sub HighlightResultUsingAnnotations(ByVal processor As PdfDocumentProcessor, ByVal result As PdfTextSearchResults)
+        Private Shared Sub HighlightResultWithAnnotations(ByVal processor As PdfDocumentProcessor, ByVal result As PdfTextSearchResults)
+            Dim facade As PdfDocumentFacade = processor.DocumentFacade
+            Dim page As PdfPageFacade = facade.Pages(result.Page.GetPageIndex())
+
             For i As Integer = 0 To result.Rectangles.Count - 1
-                Dim annotation As PdfTextMarkupAnnotationData = processor.AddTextMarkupAnnotation(result.PageNumber, result.Rectangles(i), PdfTextMarkupAnnotationType.Highlight)
+                Dim annotation As PdfTextMarkupAnnotationFacade = page.AddTextMarkupAnnotation(result.Rectangles(i), PdfTextMarkupAnnotationType.Highlight)
                 If annotation IsNot Nothing Then
                     annotation.Color = New PdfRGBColor(0.2, 0.6, 0)
                 End If
             Next
         End Sub
 
-        Private Class CSharpImpl
-
-            <System.Obsolete("Please refactor calling code to use normal Visual Basic assignment")>
-            Shared Function __Assign(Of T)(ByRef target As T, value As T) As T
-                target = value
-                Return value
-            End Function
-        End Class
+        'Helper function to assign and return value in Do While loop
+        Private Shared Function InlineAssignHelper(Of T)(ByRef target As T, ByVal value As T) As T
+            target = value
+            Return value
+        End Function
     End Class
 End Namespace
