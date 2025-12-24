94c00692cd5004e27da8f7490351ad6b4a8ea1f4
DevExpressExampleBot <fakeEmail>
2025-12-25 03:09:11 +0400

    autogenerate VB

--- VB/HighlightSearchResults/Properties/AssemblyInfo.vb
+++ VB/HighlightSearchResults/Properties/AssemblyInfo.vb
@@ -1,5 +1,5 @@
 Imports System.Reflection
-Imports System.Runtime.InteropServices
+Imports System.Runtime.CompilerServices
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
